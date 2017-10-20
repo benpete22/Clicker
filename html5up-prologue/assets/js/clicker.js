002dc568b484f17ba3f20b74cdabb7702327ed03
benpete22 <ben@benpete.com>
2017-10-19 19:45:30 -0500

    Initial commit

---
+++ html5up-prologue/assets/js/clicker.js
@@ -0,0 +1,94 @@
+var clicks = 0;
+var totalClicks = 0;
+var autoClick1Count = 0;
+var autoClick1MultiCount = 1;
+var masterMulti = 1;
+
+        
+
+    
+    function update(num){
+        if (num<0){clicks = clicks + num}
+        else{
+        clicks = clicks + num*masterMulti;
+        totalClicks = totalClicks + num*masterMulti;
+        }
+        $("#clicks").html(numeral(Math.floor(clicks)).format('0.[000]a'))
+        $("#totalClicks").html(numeral(Math.floor(totalClicks)).format('0.[000]a'))
+
+    }
+    
+    function autoClick1(status){
+        //clicks per run
+        var autoclicks = 1
+        autoclicks = autoclicks * autoClick1MultiCount;
+        //duration of auto click
+        var time = 5000/autoClick1Count
+        
+        if (autoClick1Count === 0){ status="continue"}
+        if (status=="continue"){
+            autoClick1Time = setTimeout(function(){ update(autoclicks); autoClick1("continue");}, time); 
+        }
+    }
+
+    
+    
+    
+    
+    $("#clicker").click(function(){
+        update(1);
+    });
+
+    
+    $("#autoClick1").click(function(){
+        //base price
+        var bPrice = 10 
+        var scale = 1.2
+        var price = bPrice
+       
+        function calcPrice(){
+            price = bPrice
+            for (i = 0; i < autoClick1Count; i++) {
+                price = price*scale
+            }
+            price = Math.floor(price)
+            return price
+        }
+        price = calcPrice()
+        
+        if (clicks >= price){
+            update(-price)
+            autoClick1()
+            autoClick1Count ++
+            $("#autoClicker1Price").html(numeral(calcPrice()).format('0.[000]a'))
+            $("#autoClicker1Count").html(autoClick1Count)
+        }
+        
+        
+    });
+    $("#autoClick1Multi").click(function(){
+        //base price
+        var bPrice = 200 
+        var scale = 1.35
+        var price = bPrice
+       
+        function calcPrice(){
+            price = bPrice
+            for (i = 0; i < autoClick1MultiCount; i++) {
+                price = price*scale
+            }
+            price = Math.floor(price)
+            return price
+        }
+        price = calcPrice()
+        
+        if (clicks >= price){
+            update(-price)
+            autoClick1MultiCount ++
+            $("#autoClicker1MultiPrice").html(numeral(calcPrice()).format('0.[000]a'))
+            $("#autoClicker1MultiCount").html(autoClick1MultiCount)
+        }
+    });
+        
+        
+  
